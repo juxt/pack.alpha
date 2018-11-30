b591f4512b191c6e4ab63ac6c7e19db3731b39e9
Dominic Monroe <dominic@juxt.pro>
2018-11-30 15:09:24 +0000

    Add vfs abstraction

--- src/mach/pack/alpha/impl/elodin.clj
+++ src/mach/pack/alpha/impl/elodin.clj
@@ -3,32 +3,10 @@
 
   Provides functionality for naming a file suitable for use in archives."
   (:require
-   [clojure.java.io :as io])
+   [clojure.java.io :as io]
+   [clojure.string :as string])
   (:import
    [java.nio.file Files Paths]))
-
-(defn- signature
-  [algorithm]
-  (javax.xml.bind.DatatypeConverter/printHexBinary (.digest algorithm)))
-
-(defn- consume-input-stream
-  [input-stream]
-  (let [buf-n 2048
-        buffer (byte-array buf-n)]
-    (while (> (.read input-stream buffer 0 buf-n) 0))))
-
-(defn- sha256
-  [file]
-  (.getMessageDigest
-    (doto
-      (java.security.DigestInputStream.
-        (io/input-stream file)
-        (java.security.MessageDigest/getInstance "SHA-256"))
-      consume-input-stream)))
-
-(defn hash-derived-name
-  [file]
-  [(str (signature (sha256 file)) "-" (.getName file))])
 
 (defn- paths-get
   [[first & more]]
@@ -66,3 +44,39 @@
 (defn full-path-derived-name
   [file]
   (file->path-seq file))
+
+(def interpolation-re #"((\{(.+?)\})\$|\$(\{(.+?)\})|%)")
+
+(defmacro ^:private <<
+  [s & args]
+  `(format
+     ~(string/replace s interpolation-re "%s")
+     ~@(map (fn [[_ _ _ prefix _ suffix] x]
+              `(if-let [x# ~x]
+                 (str ~prefix x# ~suffix)
+                 ""))
+            (re-seq interpolation-re s)
+            args)))
+
+(defn versioned-lib
+  [{:keys [lib] :as all}]
+  (<< "%{__}${__}$"
+      (namespace lib)
+      (name lib)
+      (or ((some-fn :mvn/version :sha) all))))
+
+(defn directory-unique
+  [{:keys [path deps/root]}]
+  (string/join
+    "-"
+    (path->path-seq
+      (.relativize (str->abs-path root)
+                   (str->abs-path path)))))
+
+(defn jar-name
+  [all]
+  (str (versioned-lib all) ".jar"))
+
+(defn directory-name
+  [all]
+  (str (versioned-lib all) "-" (directory-unique all)))
