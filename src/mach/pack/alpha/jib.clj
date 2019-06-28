efa2bdd2345e6bf5dab8ad2d0fe987a206c003d7
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2019-06-28 23:38:29 +0300

    Add project files, support tar, docker and registry

--- src/mach/pack/alpha/jib.clj
+++ src/mach/pack/alpha/jib.clj
@@ -1,39 +1,62 @@
 (ns mach.pack.alpha.jib
   (:require [mach.pack.alpha.impl.tools-deps :as tools-deps]
-            [mach.pack.alpha.impl.lib-map :as lib-map]
             [clojure.string :as str]
-            [clojure.tools.cli :as cli]
-            [mach.pack.alpha.impl.elodin :as elodin])
+            [clojure.tools.cli :as cli])
   (:import (com.google.cloud.tools.jib.api Jib AbsoluteUnixPath)
-           (java.nio.file Paths)
+           (java.nio.file Paths Files LinkOption)
            (com.google.cloud.tools.jib.api LayerConfiguration
-                                           LayerConfiguration$Builder
                                            Containerizer
-                                           DockerDaemonImage)))
+                                           DockerDaemonImage
+                                           TarImage
+                                           RegistryImage
+                                           ImageReference)
+           (com.google.cloud.tools.jib.frontend CredentialRetrieverFactory)))
 
-(defn jib [{::tools-deps/keys [lib-map]} {:keys [image-name base-image target-dir]}]
+(def string-array (into-array String []))
+(def target-dir "/home/app")
+
+(defn jib [{::tools-deps/keys [paths lib-map]} {:keys [image-name image-type tar-file base-image target-dir main]}]
   (println "Building" image-name)
-  (let [libs-layer (.build (reduce (fn [acc {:keys [path] :as all}]
-                                     (.addEntry acc
-                                                (Paths/get path (into-array String []))
-                                                (AbsoluteUnixPath/get (str target-dir
-                                                                           "/"
-                                                                           (elodin/versioned-lib all)
-                                                                           ".jar"))))
-                                   (LayerConfiguration/builder)
-                                   (lib-map/lib-jars lib-map)))]
-    (-> (Jib/from base-image)
-        (.addLayer libs-layer)
-        (.setEntrypoint (into-array String ["java" "-cp" (str target-dir "/*") "clojure.main"]))
-        (.containerize (Containerizer/to (DockerDaemonImage/named image-name))))))
+  (-> (Jib/from base-image)
+      (.addLayer (into []
+                       (map #(Paths/get % string-array))
+                       (mapcat :paths (vals lib-map)))
+                 (AbsoluteUnixPath/get target-dir))
+      (.addLayer (-> (reduce (fn [acc project-path]
+                               (let [path (Paths/get project-path string-array)]
+                                 (if (Files/exists path (into-array LinkOption []))
+                                   (.addEntryRecursive acc
+                                                       path
+                                                       (AbsoluteUnixPath/get (str target-dir "/" project-path)))
+                                   acc)))
+                             (LayerConfiguration/builder)
+                             paths)
+                     (.build)))
+      (.setEntrypoint (into-array String ["java"
+                                          "-cp" (str/join ":" (cons (str target-dir "/*")
+                                                                    (map #(str target-dir "/" %) paths)))
+                                          "clojure.main" "-m" main]))
+      (.containerize (Containerizer/to (case image-type
+                                         :docker (DockerDaemonImage/named image-name)
+                                         :tar (-> (TarImage/named image-name)
+                                                  (.saveTo (Paths/get tar-file string-array)))
+                                         :registry (-> (RegistryImage/named image-name)
+                                                       (.addCredentialRetriever (-> (CredentialRetrieverFactory/forImage (ImageReference/parse image-name))
+                                                                                    (.dockerConfig)))))))))
+
+(def image-types #{:docker :registry :tar})
 
 (def ^:private cli-options
   (concat
-   [[nil "--image-name IMAGE" "Name of image to build for docker daemon"]
+   [[nil "--image-name NAME" "Name of the image"]
+    [nil "--image-type TYPE" (str "Type of the image, one of: " (str/join ", " (map name image-types)))
+     :parse-fn keyword
+     :validate [image-types (str "Supported image types: " (str/join ", " (map name image-types)))]
+     :default :docker]
+    [nil "--tar-file FILE" "Tarball file name"]
     [nil "--base-image BASE-IMAGE" "Base Docker image to use"
-     :default "openjdk:11-jre-slim"]
-    [nil "--target-dir PATH" "Where to place resources in the Docker image"
-     :default "/home/app/lib"]]
+     :default "gcr.io/distroless/java:11"]
+    ["-m" "--main SYMBOL" "Main namespace"]]
    tools-deps/cli-spec
    [["-h" "--help" "show this help"]]))
 
@@ -57,8 +80,10 @@
 (defn -main [& args]
   (let [{{:keys [help
                  image-name
+                 image-type
+                 tar-file
                  base-image
-                 target-dir]
+                 main]
           :as options} :options
          :as parsed-opts} (cli/parse-opts args cli-options)
         errors (:errors parsed-opts)]
@@ -72,4 +97,7 @@
                (tools-deps/parse-deps-map options))
            {:base-image base-image
             :target-dir target-dir
-            :image-name image-name}))))
+            :image-name image-name
+            :image-type image-type
+            :tar-file tar-file
+            :main main}))))
