5d615bc578ce2f81eddf9f208182d14e69e194c2
Fernando Dobladez <fernando@code54.com>
2019-11-17 22:51:20 -0300

    Document -D EDN (--sdeps EDN) to pass extra deps map to tools.deps

--- src/mach/pack/alpha/impl/tools_deps.clj
+++ src/mach/pack/alpha/impl/tools_deps.clj
@@ -43,9 +43,7 @@
    ["-e" "--extra-path STRING" "Add directory to classpath for building. Same as :extra-paths"
     :assoc-fn (fn [m k v] (update m k conj v))
     :id ::extra]
-
-
-   ["-D" "--sdeps EDN" "Deps data to use as the last deps file to be merged by tool.deps when pulling dependencies for image. Equivalent to `clj -Sdeps EDN`."
+   ["-D" "--sdeps EDN" "Deps data to use as the last deps map to be merged by tool.deps when pulling dependencies at build time. Equivalent to `clj -Sdeps EDN`."
     :id ::sdeps
     :parse-fn edn/read-string
     :default "{}"]
